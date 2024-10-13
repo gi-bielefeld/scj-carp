7f93e47e2b23ece967b7874db8e6ffcd89c20a6d
Leo B <seepevzofunibi>
2024-10-13 14:31:31 +0200

    Implement adjacency output

--- scj_carp.py
+++ scj_carp.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 from utils import *
 import sys
 
@@ -58,6 +58,7 @@ def calc_carp_splits(mbpg,colors):
             splits[sid] = 0
         splits[sid]+=1
     #splits_stupid_case = dict([(k,0) for k in splits])
+    return splits
     '''
     for k in splits:
         X=id_to_set(k)
@@ -85,7 +86,7 @@ def calc_carp_splits(mbpg,colors):
         #print("Split changed by {}".format(val))
         splits[k]+=val
     '''
-    return splits
+    
 
 
 def calc_carp_splits_alt_alt(mbpg,colors):
@@ -181,15 +182,22 @@ def calc_carp_splits_alt(mbpg,colors,threshold=0):
 
             
             
-def calc_carp_index(mbpg):
+def calc_carp_index(mbpg,get_edge_partition=False):
+    if get_edge_partition:
+        uncontested=[]
+        contested = []
     carp_index=0
     for u,v in mbpg.edges():
         clsu = [mbpg[u][x]['colors'] for x in mbpg[u] if x!=v]
         clsv = [mbpg[v][x]['colors'] for x in mbpg[v] if x!=u]
         if len(clsu)==0 and len(clsv)==0:
+            if get_edge_partition:
+                uncontested.append((u,v))
             continue
+        if get_edge_partition:
+            contested.append((u,v))
         carp_index+=1
-    return carp_index
+    return carp_index if (not get_edge_partition) else (carp_index,contested,uncontested)
 
     
 
@@ -208,11 +216,37 @@ def print_splits(splits,top=-1):
 def print_mbpg(mbpg):
     for u,v,colors in mbpg.edges(data='colors'):
         print(u,v,colors)
+
+def filter_tree(splits):
+    tree = []
+    for v,k in sorted([(v,k) for k,v in splits.items()],reverse=True):
+        s = id_to_set(k)
+        compatible=True
+        for so,_ in tree:
+            if len(s.intersection(so))!=0  and not (s.issubset(so) or so.issubset(s)):
+                compatible=False
+                break
+        if compatible:
+            tree.append((k,v))
+    return tree
+
+def predict_adjacencies(tree,mg):
+    pass
+
+def partition_adjacencies(mbpg,colorset):
+    ambpg = proj_to_color(mbpg,colorset)
+    _,contested,uncontested = calc_carp_index(ambpg,get_edge_partition=True)
+    return contested,uncontested
+
 def main():
     parser=ArgumentParser()
     parser.add_argument('unimog')
     parser.add_argument('--alt',action='store_true',help='Activate alternative split calculation')
     parser.add_argument("--core",action='store_true')
+    parser.add_argument("--greedy-tree",action='store_true')
+    parser.add_argument("--candidate-adjacencies",action='store_true')
+    parser.add_argument("--write-measure",type=FileType('w'))
+    parser.add_argument("--write-carp-adjacencies",type=FileType('w'))
     args=parser.parse_args()
     with open(args.unimog) as f:
         gnms = readGenomes(f.readlines())
@@ -225,12 +259,22 @@ def main():
             print(x,tlen,file=sys.stderr)
         colors = [x for x,_ in gnms]
         mbpg = build_multi_bp_graph(gnms)
-        print("Carp index: %d"%calc_carp_index(mbpg))
-        #print_mbpg(mbpg)
-        if args.alt:
-            splits=calc_carp_splits_alt_alt(mbpg,colors)
+        if args.write_carp_adjacencies:
+            ci,_,uc= calc_carp_index(mbpg,get_edge_partition=True)
+
+            for u,v in uc:
+                un,ux = u
+                vn,vx = v
+                print("{un} {ux}\t{vn} {vx}".format(un=un,ux=ux,vn=vn,vx=vx),file=args.write_carp_adjacencies)
         else:
-            splits = calc_carp_splits(mbpg,colors)
-            print_splits(splits)
+            ci=calc_carp_index(mbpg)
+        print("Carp index: %d"%ci, file=args.write_measure if args.write_measure else sys.stdout)
+
+        #print_mbpg(mbpg)
+        #if args.alt:
+        #    splits=calc_carp_splits_alt_alt(mbpg,colors)
+        #else:
+        #    splits = calc_carp_splits(mbpg,colors)
+        #    print_splits(splits)
 
 main()
